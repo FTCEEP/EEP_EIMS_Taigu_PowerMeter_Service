68bbd239fbaac68875dea9d8e03b67c7e78e7314
FTCEEP <ftceepserver@gmail.com>
2015-08-07 17:37:26 +0800

    Update Form1.vb
    222

--- Form1.vb
+++ Form1.vb
@@ -27,7 +27,7 @@
         Try
 
             Timer01.Text = CInt(Timer01.Text) + 1
-            System.Threading.Thread.Sleep(6000)
+            System.Threading.Thread.Sleep(6001)
             'BackgroundWorker1.RunWorkerAsync()
             'Timer1.Enabled = False
         Catch ex As Exception
